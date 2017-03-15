b1151744310a8bc58a841c05da0ccddc85aa35f3
Sergio Pantoja <sergio@sergiopantoja.com>
2017-03-15 11:15:13 -0400

    Add working MVP of Safari Extension

--- safari-extension/ShareViewController.swift
+++ safari-extension/ShareViewController.swift
@@ -8,24 +8,63 @@
 
 import UIKit
 import Social
+import MobileCoreServices
 
 class ShareViewController: SLComposeServiceViewController {
 
+    let host  = "https://youricebox.com" // must be an HTTPS endpoint
+    let email = "you@gmail.com"
+    let key   = "abc123"
+
     override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
         return true
     }
 
+    override func configurationItems() -> [Any]! {
+        return []
+    }
+
     override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        let extensionItem = extensionContext?.inputItems.first as! NSExtensionItem
+        let itemProvider = extensionItem.attachments?.first as! NSItemProvider
+        let propertyList = String(kUTTypePropertyList)
+
+        if itemProvider.hasItemConformingToTypeIdentifier(propertyList) {
+            itemProvider.loadItem(forTypeIdentifier: propertyList, options: nil, completionHandler: { (item, error) -> Void in
+                let dictionary = item as! NSDictionary
+                OperationQueue.main.addOperation {
+                    let results = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    let urlString = results["currentUrl"] as? String
+                    self.postData(title: self.contentText, url: urlString!)
+                }
+            })
+        }
+
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    func postData(title: String, url: String) {
+        let dict = ["item": ["title": title, "url": url]] as [String: Any]
+        if let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: []) {
+
+            let url = NSURL(string: "\(self.host)/items")!
+            let request = NSMutableURLRequest(url: url as URL)
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue(self.email, forHTTPHeaderField: "X-Api-Email")
+            request.addValue(self.key, forHTTPHeaderField: "X-Api-Key")
+
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
+                if error != nil {
+                    print(error?.localizedDescription ?? "Unknown Error")
+                    return
+                }
+            }
+
+            task.resume()
+        }
     }
 
 }
